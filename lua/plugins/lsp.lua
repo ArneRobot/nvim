3a99879d00d7f3911a8cc9508116455b8cc2c2ec
ArneRobot <brede.bs04@gmail.com>
2024-02-28 20:11:31 +0100

    lsp fungerer, kan ikke seg .filer med oil.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,72 +1,32 @@
 
 return {
-    "williamboman/mason.nvim",
-    dependencies = {
-    },
+    'VonHeikemen/lsp-zero.nvim',
     config = function()
-        require("mason").setup()
-    end
-}
+        local lsp = require('lsp-zero').preset({
+            name = 'minimal',
+            set_lsp_keymaps = true,
+            manage_nvim_cmp = true,
+            suggest_lsp_servers = false,
+        })
 
---[[
-'VonHeikemen/lsp-zero.nvim',
-config = function()
-require("lsp-zero").preset({
-name = "minimal",
-set_lsp_keymap = true,
-manage_nvim_cmp = true,
-suggest_lsp_servers = true,
-}).nvim_workspace().setup()
-end,
-opts = {
-mapping = {
-["<CR>"] = require("cmp.mapping").confirm({ select = false }),
-}
-}
-return {
-{
-'VonHeikemen/lsp-zero.nvim',
-opts = {
-maping = {
-["<CR>"] = cmp.mapping.confirm({ select = false }),
-}
-},
-config = function() require("lsp-zero").preset({
-name = "minimal",
-set_lsp_keymap = true,
-manage_nvim_cmp = true,
-suggest_lsp_servers = true,
-}).nvim_workspace().setup() end,
-}
-}
-local lsp = require("lsp-zero").preset({
-name = "minimal",
-set_lsp_keymap = true,
-manage_nvim_cmp = true,
-suggest_lsp_servers = true,
-})
-local cmp = require("cmp")
+        lsp.nvim_workspace()
+        lsp.setup()
 
-lsp.nvim_workspace()
-lsp.setup()
+        local cmp = require('cmp')
 
-cmp.setup({
-maping = {
-["<CR>"] = cmp.mapping.confirm({ select = false }),
-}
-})
-vim.diagnostic.config({
-virtual_text = true,
-signs = true,
-update_in_insert = false,
-underline = true,
-severity_sort = false,
-float = true,
-})
+        cmp.setup({
+            mapping = {
+                ['<CR>'] = cmp.mapping.confirm({select = false}),
+            }
+        })
 
-return {
-{
---'VonHeikemen/lsp-zero.nvim',
+        vim.diagnostic.config({
+            virtual_text = true,
+            signs = true,
+            update_in_insert = false,
+            underline = true,
+            severity_sort = false,
+            float = true,
+        })
+    end,
 }
-}
-]]--
