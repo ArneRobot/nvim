0dec3b199e2982466f6d1b7ebefb79d263f9910b
ArneRobot <brede.bs04@gmail.com>
2024-02-28 21:08:47 +0100

    NÅ er alt fint

--- lua/plugins/git_config.lua
+++ lua/plugins/git_config.lua
@@ -10,5 +10,9 @@ return {
 
     {
         "tpope/vim-fugitive",
+        config = function()
+            vim.keymap.set("n", "<leader>ga", ":Git add %<CR>", {})
+            vim.keymap.set("n", "<leader>gc", ":Git commit -m ''<left>", {})
+        end
     }
 }
